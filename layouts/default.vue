f72505396bbd214ff157c8611993c6ec571a14a9
André Weller <info@lost.design>
2019-05-12 01:59:58 +0200

    add tracker

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,19 @@ export default {
     Logo,
     Search,
     Sidebar
+  },
+  created(){
+    (function(f, a, t, h, o, m){
+      a[h]=a[h]||function(){
+        (a[h].q=a[h].q||[]).push(arguments)
+      };
+      o=f.createElement('script'),
+      m=f.getElementsByTagName('script')[0];
+      o.async=1; o.src=t; o.id='fathom-script';
+      m.parentNode.insertBefore(o,m)
+    })(document, window, '//stats.lost.services/tracker.js', 'fathom');
+    fathom('set', 'siteId', 'KQYXI');
+    fathom('trackPageview');
   }
 }
 </script>
