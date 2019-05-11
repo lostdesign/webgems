cf1eee367a55e52bb72c41ace8d2bca679311859
André Weller <info@lost.design>
2019-05-12 01:44:04 +0200

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
+      { name:'theme-color', content: '#ffffff' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      { rel:'manifest', href:'/site.webmanifest' },
+      { rel:'icon', type:'image/png', sizes:'16x16', href:'/favicon-16x16.png' },
+      { rel:'icon', type:'image/png', sizes:'32x32', href:'/favicon-32x32.png' },
+      { rel:'apple-touch-icon', sizes:'76x76', href:'/apple-touch-icon.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
