9f3f9b13f7962276ef51a852ceb58f1e34f34714
André Weller <info@lost.design>
2019-05-12 02:31:51 +0200

    update startpage

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,15 @@ div
     p As of now, it's a curated list of resources for developers and designers.
     p If you want to suggest a resource, go ahead and contact me&nbsp;
       a(href="https://twitter.com/lostdesign") @lostdesign&nbsp;
-      //- | or join&nbsp;
+      | or lost#0001 on discord
+
       //- a(href="https://discord.gg/Bjgjdrr") my discord server
+    hr
+    p Please note that this project has just launched some days ago - many of the resources i have in my backlog have not been added yet.
+    p I plan on adding more contents first, improve the site and then slap a backend behind it, were you can submit resources and vote on others.
+    p It will also be able to manage your own favorite resources or mark some as done.
+    hr
+    p Thank you :)
 
 </template>
 
