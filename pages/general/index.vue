02a5adb343a0c6726824f233d3c6730b71b97b5f
André Weller <info@lost.design>
2019-06-15 02:31:53 +0200

    shorten desc

--- pages/general/index.vue
+++ pages/general/index.vue
@@ -30,7 +30,7 @@ export default {
         },
         {
           title: 'Developer Roadmap',
-          desc: 'Below you find a set of charts demonstrating the paths that you can take and the technologies that you would want to adopt in order to become a frontend, backend or a devops. I made these charts for an old professor of mine who wanted something to share with his college students to give them a perspective; sharing them here to help the community.',
+          desc: 'Below you find a set of charts demonstrating the paths that you can take and the technologies that you would want to adopt in order to become a frontend, backend or a devops.',
           url: 'https://github.com/kamranahmedse/developer-roadmap/blob/master/readme.md'
         },
         {
