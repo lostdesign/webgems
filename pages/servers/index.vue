90b50220a4d0a9f26686b71eb79f5df373836a23
lost.design <info@lost.design>
2019-06-16 08:22:26 +0200

    Revert "Merge pull request #54 from S3B4S/dynamic-routing"
    This reverts commit f13f012c29e84f81498442d3c4c557824864a1c1, reversing
    changes made to bc0aab42171d61ca2f19e8987a893250d40d196c.

---
+++ pages/servers/index.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  div
+    h1 Servers
+    .cards
+      template(v-for='resource in resources')
+        Card(:title='resource.title' :desc='resource.desc' :url='resource.url')
+</template>
+
+<script>
+import Card from '../../components/Card'
+
+export default {
+  data() {
+    return {
+      resources: [
+        {
+          title: 'Netlify',
+          desc: 'Free static hosting with privacy in mind!',
+          url: 'https://netlify.com'
+        },
+        {
+          title: 'Github Pages',
+          desc: 'Free static hosting by Github.',
+          url: 'https://pages.github.com'
+        },
+        {
+          title: 'Namecheap',
+          desc: 'Domains for your projects',
+          url: 'https://namecheap.com'
+        },
+        {
+          title: 'Servers for hackers',
+          desc: 'Tutorials on how to handle your servers.',
+          url: 'https://serversforhackers.com/'
+        },
+      ]
+    }
+  },
+  components: {
+    Card
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
