21ccb787f61ece9ba927688585fa3ea44237928d
Unknown <Kevinsebastianate@hotmail.com>
2019-06-17 17:13:04 +0200

    Share common component & filter data

---
+++ components/CategoryPage.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div
+    h1 {{ resources.title }}
+    .cards
+      template(v-for='resource in resources.resources')
+        Card(:title='resource.title' :desc='resource.desc' :url='resource.url')
+</template>
+
+<script>
+import Card from './Card'
+
+export default {
+  props: ["resources"],
+  components: {
+    Card
+  },
+}
+</script>
+
---
+++ pages/communities/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  CategoryPage(:resources="resources")
+</template>
+
+<script>
+import CategoryPage from '../../components/CategoryPage'
+import store from '../../store.json'
+
+export default {
+  data() {
+    return {
+      resources: store.find(x => x.title.toLowerCase() === 'communities')
+    }
+  },
+  components: {
+    CategoryPage
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
