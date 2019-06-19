f1dff7afe2db2f0a4b0bdb7e5da48c8c22099195
lostdesign <info@lost.design>
2019-06-20 01:45:28 +0200

    remove product hunt, add author meme, fix github badge

--- components/Github.vue
+++ components/Github.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  a(href="https://github.com/lostdesign/webgems" class="github-corner" aria-label="View source on GitHub" target='webgems-github-repo' rel='noreferrer')
+  a(href="https://github.com/webgems/webgems" class="github-corner" aria-label="View source on GitHub" target='webgems-github-repo' rel='noreferrer')
     svg(width="80" height="80" viewBox="0 0 250 250" style="fill:#08e5ff; color:#232331; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true")
       path(d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z")
       path(d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm")
