d20423c210e80193cc65b2ef25e209f529feaf02
lostdesign <info@lost.design>
2019-06-20 12:11:32 +0200

    setup i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/i18n.js'
   ],
 
   /*
---
+++ store/store.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+  locales: ['en', 'fr', 'de'],
+  locale: 'en'
+})
+
+export const mutations = {
+  SET_LANG(state, locale) {
+    if (state.locales.indexOf(locale) !== -1) {
+      state.locale = locale
+    }
+  }
+}
