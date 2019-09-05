1e499c7758a02aa0cd1dfc94fb5f64fb3fd3c08c
Kevin <kevinsebastianate@hotmail.com>
2019-09-05 21:45:02 +0200

    Remove console.log

--- store/Sidebar.js
+++ store/Sidebar.js
@@ -8,7 +8,6 @@ export const getters = {
 
 export const mutations = {
   toggleCardsShown(state) {
-    console.log(state.cardsShown)
     if (process.browser) localStorage.setItem('cardsShown', !state.cardsShown)
     state.cardsShown = !state.cardsShown
   }
