8fe5329b2e284fa32a286d8ac19e4a5aa9d672c5
Johan-Ng <44483400+Johan-Ng@users.noreply.github.com>
2019-10-13 23:51:09 +0800

    re opened

---
+++ resources/index.js
@@ -0,0 +1,37 @@
+import css from './css'
+import daily from './daily'
+import design from './design'
+import frontend from './frontend'
+import fullstack from './fullstack'
+import general from './general'
+import html from './html'
+import javascript from './javascript'
+import php from './php'
+import podcasts from './podcasts'
+import python from './python'
+import ruby from './ruby'
+import server from './server'
+import utility from './utility'
+import icons from './icons'
+import accessibility from './accessibility'
+import contentCreators from './contentCreators'
+
+export default [
+  css,
+  daily,
+  design,
+  frontend,
+  fullstack,
+  general,
+  html,
+  javascript,
+  php,
+  podcasts,
+  python,
+  ruby,
+  server,
+  utility,
+  icons,
+  accessibility,
+  contentCreators,
+]
