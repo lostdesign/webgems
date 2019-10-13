0fe881c21a4b24092b4c7af6015fa27ad4529c30
Jonas Gierer <jonas@gierer.xyz>
2019-10-13 15:44:23 +0200

    Add Typography category

--- resources/index.js
+++ resources/index.js
@@ -14,6 +14,7 @@ import server from './server'
 import utility from './utility'
 import icons from './icons'
 import accessibility from './accessibility'
+import typography from './typography'
 
 export default [
   css,
@@ -32,4 +33,5 @@ export default [
   utility,
   icons,
   accessibility,
+  typography,
 ]
