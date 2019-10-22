1197f7a22afc29d859b00e1ba7f3b2871bbcaea6
nautatva <nnautatva@gmail.com>
2019-10-22 11:23:37 +0530

    removed phpcodechecker, added og tags, description

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,7 @@ import Github from '../components/Github'
 import Logo from '../components/Logo'
 import Search from '../components/Search'
 import Sidebar from '../components/Sidebar'
+import pkg from '../package'
 
 export default {
   components: {
@@ -23,6 +24,8 @@ export default {
   head () {
     return {
       meta: [
+        { hid: 'og:title', property: 'og:title', content: pkg.name },
+        { hid: 'og:description', property: 'og:description', content: pkg.description },
         { hid: 'og:image', property: 'og:image', content: this.prodBaseUrl + 'apple-touch-icon.png' },
         { hid: 'og:url', property: 'og:url', content: this.prodBaseUrl },
       ],
