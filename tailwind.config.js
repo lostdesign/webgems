d89701d48a8476eaf7339e235b243e1bf8974341
Abdalla Arbab <the94air@users.noreply.github.com>
2020-08-28 10:45:50 +0200

    :tada: init commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {},
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  },
+  future: {
+    removeDeprecatedGapUtilities: true
+  }
+}
