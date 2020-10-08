f9f1ae011e680a1d502b7c84937d5894d4d82ed7
wellá <info@lost.design>
2020-10-08 17:12:42 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   main
     h1.text-black.font-bold.text-xl.mb-2 What is&nbsp;
       span 💎 webgems?
-    p Webgems is a goto place for devs and designers to find new resources and more. As a beginner, it's sometimes not easy to find what you need since you don't know where to look for what.
+    p webgems is a goto place for devs and designers to find new resources and more. As a beginner, it's sometimes not easy to find what you need since you don't know where to look for what.
     p Therefore we created this project to have the most common and best resources for beginners and veterans in one place — kind of like a bookmarks list for devs and designers, containing only the best gems out there.
     p If you want to add your gem, go ahead and open a pull request on GitHub (click on that octocat at the top right corner).
     p By the way, this project lives by getting shared; if you find this worthy, please share it with others.
